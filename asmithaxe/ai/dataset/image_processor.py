20e553b0398d03364dcca4173b1bc77f0c7116fc
asmithaxe <github.com@ouvert.org>
2021-03-09 11:07:33 +1000

    Fix cropping and naming errors that caused overwritten images.

--- asmithaxe/ai/dataset/image_processor.py
+++ asmithaxe/ai/dataset/image_processor.py
@@ -58,11 +58,6 @@ class CroppingImageProcessor(ImageProcessor):
     for each object.
     """
 
-    def __init__(self, image_processors, image_patch_height, image_patch_width):
-        super().__init__(image_processors=image_processors)
-        self.image_patch_height = image_patch_height
-        self.image_patch_width = image_patch_width
-
     def process(self, image, image_annotation, object_annotations):
         split_filename = os.path.splitext(image_annotation.image_filename)
         filename_prefix = split_filename[0]
@@ -70,35 +65,28 @@ class CroppingImageProcessor(ImageProcessor):
         crop_count = 0
         for object_annotation in object_annotations:
 
-            # Verify that the patch area fits within the image, otherwise ignore the annotation.
-            if object_annotation.xmin > self.image_patch_width // 2 and \
-                    object_annotation.ymin > self.image_patch_height // 2 and \
-                    object_annotation.xmax + self.image_patch_width // 2 < image_annotation.image_width and \
-                    object_annotation.ymax + self.image_patch_height // 2 < image_annotation.image_height:
-                # Calculate the cropping parameters.
-                min_x = object_annotation.xmin - (self.image_patch_width // 2)
-                max_x = object_annotation.xmax + (self.image_patch_width // 2)
-                min_y = object_annotation.ymin - (self.image_patch_height // 2)
-                max_y = object_annotation.ymin + (self.image_patch_height // 2)
+            # Crop the image.
+            cropped_image = image.crop((object_annotation.xmin, object_annotation.ymin, object_annotation.xmax,
+                                        object_annotation.ymax))
 
-                # Crop the image.
-                cropped_image = image.crop((min_x, min_y, max_x, max_y))
+            # Build the new ImageAnnotation and ObjectAnnotation for the cropped image.
+            new_image_annotation = ImageAnnotation(
+                image_filename=filename_prefix + '_' + str(crop_count) + filename_suffix,
+                image_width=image_annotation.image_width,
+                image_height=image_annotation.image_height
+            )
+            new_object_annotation = ObjectAnnotation(class_id=object_annotation.class_id,
+                                                     xmin=0,
+                                                     xmax=object_annotation.xmax - object_annotation.xmin,
+                                                     ymin=0,
+                                                     ymax=object_annotation.ymax - object_annotation.ymin)
 
-                # Build the new ImageAnnotation and ObjectAnnotation for the cropped image.
-                new_image_annotation = ImageAnnotation(
-                    image_filename=filename_prefix + '_' + str(crop_count) + filename_suffix,
-                    image_width=image_annotation.image_width,
-                    image_height=image_annotation.image_height
-                )
-                new_object_annotation = ObjectAnnotation(class_id=object_annotation.class_id,
-                                                         xmin=0,
-                                                         xmax=self.image_patch_width,
-                                                         ymin=0,
-                                                         ymax=self.image_patch_height)
+            # Invoke the next ImageProcessor(s).
+            for image_processor in self.image_processors:
+                image_processor(cropped_image, new_image_annotation, [new_object_annotation])
 
-                # Invoke the next ImageProcessor(s).
-                for image_processor in self.image_processors:
-                    image_processor(cropped_image, new_image_annotation, [new_object_annotation])
+            # Increment the counter.
+            crop_count += 1
 
 
 ########################################################################################################################
